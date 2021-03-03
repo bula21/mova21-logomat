990eee15dfe7cf28a5c1602218f3edce5fe694ab
Andreas Hotz / Selva <77546423+logmat@users.noreply.github.com>
2021-03-03 07:55:13 +0100

    Feature/material order total (#11)
    * add order item total
    
    * add order total

--- src/components/MaterialOrderList.vue
+++ src/components/MaterialOrderList.vue
@@ -37,6 +37,9 @@
         <template v-slot:item.return="{ item }">
           <span>{{ shortDate(item.return) }}</span>
         </template>
+        <template v-slot:item.total.amount="{ item }">
+          <span>{{ item.total.amount.toFixed(2) }}</span>
+        </template>
         <template v-slot:item.id>
           <v-icon small> mdi-pencil </v-icon>
         </template>
@@ -46,7 +49,7 @@
 </template>
 
 <script>
-import { apiAuthenticated, ApiError } from "@/lib/api";
+import { apiAuthenticated, ApiError, limit } from "@/lib/api";
 import { joinInPlace } from "@/lib/join";
 import { DateTime } from "luxon";
 
@@ -64,6 +67,7 @@ export default {
       { text: "Ausführung", value: "delivery_type.name", width: "120px" },
       { text: "Ausgabe", value: "delivery", align: "right" },
       { text: "Rücknahme", value: "return", width: "120px", align: "right" },
+      { text: "Summe", value: "total.amount", align: "right" },
       { text: "", value: "id" },
     ],
     orders: [],
@@ -82,6 +86,8 @@ export default {
           departements,
           order_types,
           delivery_types,
+          orderItems,
+          items,
         ] = await Promise.all([
           apiAuthenticated("/items/mat_order"),
           apiAuthenticated("/items/mat_state"),
@@ -89,12 +95,31 @@ export default {
           apiAuthenticated("/items/mat_departement"),
           apiAuthenticated("/items/mat_order_type"),
           apiAuthenticated("/items/mat_delivery_type"),
+          apiAuthenticated("/items/mat_order_item", limit(-1)),
+          apiAuthenticated("/items/mat_item", limit(-1)),
         ]);
         joinInPlace(orders, states, "state");
         joinInPlace(clients, departements, "departement");
         joinInPlace(orders, clients, "client");
         joinInPlace(orders, order_types, "order_type");
         joinInPlace(orders, delivery_types, "delivery_type");
+        joinInPlace(orderItems, items, "item");
+        const totalItems = orderItems.reduce((acc, item) => {
+          const thing = acc.find((group) => group.id === item.order);
+          if (thing) {
+            thing.amount += item.quantity * item.item.price;
+          } else {
+            acc.push({
+              id: item.order,
+              amount: item.quantity * item.item.price,
+            });
+          }
+          return acc;
+        }, []);
+        orders.forEach((element) => {
+          element.total = element.id;
+        });
+        joinInPlace(orders, totalItems, "total");
         this.orders = Object.freeze(orders);
       } catch (err) {
         if (err instanceof ApiError) {
