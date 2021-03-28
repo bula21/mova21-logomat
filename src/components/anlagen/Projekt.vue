338c23c7ad56d9ce6bd1f58f9c376b35f75bb34d
Lukas Heuberger <lukas.heuberger@gmail.com>
2021-03-28 22:23:46 +0200

    add link to folder to projekt

--- src/components/anlagen/Projekt.vue
+++ src/components/anlagen/Projekt.vue
@@ -17,6 +17,7 @@
           person: true,
         },
         { prop: 'ressort_betrieb' },
+        { prop: 'ordner_link' },
         { prop: 'beginn_nutzung' },
         { prop: 'ende_nutzung' },
 
@@ -30,6 +31,9 @@
       <template v-slot:prop.avanti_seite="{ item }">
         <AvantiLink :item="item" />
       </template>
+      <template v-slot:prop.ordner_link="{ item }">
+        <OrdnerLink :item="item" />
+      </template>
       <template v-slot:prop.ressort_betrieb="{ item }">
         {{ item.ressort_name }}
       </template>
@@ -40,6 +44,7 @@
 <script>
 import DescriptionTable from "@/components/DescriptionTable";
 import AvantiLink from "@/components/anlagen/AvantiLink";
+import OrdnerLink from "@/components/anlagen/OrdnerLink";
 
 export default {
   name: "Projekt",
@@ -47,6 +52,6 @@ export default {
     projekt: Object,
     title: String,
   },
-  components: { DescriptionTable, AvantiLink },
+  components: { DescriptionTable, AvantiLink, OrdnerLink },
 };
 </script>
