57247cbaca58ab3b66a5ed350e4f7c0b3188572e
Manuel Meister <news.manuelsworld@gmail.com>
2022-07-18 17:29:19 +0200

    Fix notice in dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <v-app>
-    <v-system-bar color="#ffeb00" height="auto" class="py-2" app>
+    <v-system-bar light color="#ffeb00" height="auto" class="py-2" app>
       <p class="my-0">
         <span v-if="language.indexOf('fr') > -1">
           Le "LOGomat" n'est pas tenu à jour pendant le mova, car des
           adaptations sont effectuées pendant le montage. Si vous avez des
-          questions, la centrale LOG (<a href="tel:0275102222"
+          questions, la centrale LOG (<a style="color:black" href="tel:0275102222"
             >027&nbsp;510&nbsp;22&nbsp;22</a
           >) vous aidera volontiers.
         </span>
         <span v-else>
           Der LOGomat wird während dem mova nicht aktuell gehalten, da während
           dem Aufbau Anpassungen gemacht werden. Bei Fragen hilft die
-          LOG-Zentrale (<a href="tel:0275102222">027&nbsp;510&nbsp;22&nbsp;22</a
+          LOG-Zentrale (<a style="color:black" href="tel:0275102222">027&nbsp;510&nbsp;22&nbsp;22</a
           >) gerne weiter.
         </span>
       </p>
