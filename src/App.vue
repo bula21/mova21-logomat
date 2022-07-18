8f65cc6b5765a5a1b701d4239c45cec91f07b22a
Manuel Meister <news.manuelsworld@gmail.com>
2022-07-18 16:32:50 +0200

    Implement notice of out-of-date

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <v-app>
+    <v-system-bar color="#ffeb00" height="auto" class="py-2" app>
+      <p class="my-0">
+        <span v-if="language.indexOf('fr') > -1">
+          Le "LOGomat" n'est pas tenu à jour pendant le mova, car des
+          adaptations sont effectuées pendant le montage. Si vous avez des
+          questions, la centrale LOG (<a href="tel:0275102222"
+            >027&nbsp;510&nbsp;22&nbsp;22</a
+          >) vous aidera volontiers.
+        </span>
+        <span v-else>
+          Der LOGomat wird während dem mova nicht aktuell gehalten, da während
+          dem Aufbau Anpassungen gemacht werden. Bei Fragen hilft die
+          LOG-Zentrale (<a href="tel:0275102222">027&nbsp;510&nbsp;22&nbsp;22</a
+          >) gerne weiter.
+        </span>
+      </p>
+    </v-system-bar>
     <router-view />
   </v-app>
 </template>
@@ -7,5 +24,10 @@
 <script>
 export default {
   name: "App",
+  computed: {
+    language() {
+      return window?.navigator?.language ?? "de";
+    },
+  },
 };
 </script>
